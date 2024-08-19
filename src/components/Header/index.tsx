320d27e1d29b7da43459ea103eb57185afc80dfd
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-08-18 21:38:07 -0300

    :sparkles: feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import './styles.css';
+
 export default function Header(){
     return(
         <header>
-            <div>
+            <div className="ct-container">
                 <h1>Carros Top</h1>
             </div>
         </header>
